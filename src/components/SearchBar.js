e7a00d68b1f76bf8840be887f1c97670b5cc6788
Sheikh Mubashir <smubashir655@gmail.com>
2023-02-02 10:49:00 +0500

    now wokring

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -25,9 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15,
+    marginHorizontal: 15, 
     flexDirection: 'row',
-    marginBottom: 10
+    marginBottom: 10 
   },
   inputStyle: {
     flex: 1,
